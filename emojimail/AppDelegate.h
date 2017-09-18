1cba9dd9258ae76a5137bafb193662d4a706685b
Aram Sargsyan <aram.sargsyan.1997@gmail.com>
2017-09-18 21:22:13 +0400

    Initial Commit

---
+++ emojimail/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  emojimail
+//
+//  Created by Aram Sargsyan on 9/18/17.
+//  Copyright © 2017 Aram Sargsyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ emojimail/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  emojimail
+//
+//  Created by Aram Sargsyan on 9/18/17.
+//  Copyright © 2017 Aram Sargsyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
